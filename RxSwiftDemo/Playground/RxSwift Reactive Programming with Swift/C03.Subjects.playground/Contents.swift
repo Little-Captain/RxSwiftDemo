15e279e03a7cc0a4d41c0ebbfb8d3f3709a96dd7
Little-Captain <littlecaptain@foxmail.com>
2018-09-10 18:28:00 +0800

    添加C3的Challenge代码

--- RxSwiftDemo/Playground/RxSwift Reactive Programming with Swift/C03.Subjects.playground/Contents.swift
+++ RxSwiftDemo/Playground/RxSwift Reactive Programming with Swift/C03.Subjects.playground/Contents.swift
@@ -116,3 +116,91 @@ example(of: "Variable") {
         .disposed(by: disposeBag)
     varibale.value = "2"
 }
+
+example(of: "Create a blackjack card dealer using a publish subject") {
+    
+    let disposeBag = DisposeBag()
+    
+    let dealtHand = PublishSubject<[(String, Int)]>()
+    
+    func deal(_ cardCount: UInt) {
+        var deck = cards
+        var cardsRemaining: UInt32 = 52
+        var hand = [(String, Int)]()
+        
+        for _ in 0..<cardCount {
+            let randomIndex = Int(arc4random_uniform(cardsRemaining))
+            hand.append(deck[randomIndex])
+            deck.remove(at: randomIndex)
+            cardsRemaining -= 1
+        }
+        
+        // Add code to update dealtHand here
+        
+    }
+    
+    // Add subscription to dealtHand here
+    
+    
+    deal(3)
+}
+
+example(of: "Observe and check user session state using a variable") {
+    
+    enum UserSession {
+        
+        case loggedIn, loggedOut
+    }
+    
+    enum LoginError: Error {
+        
+        case invalidCredentials
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    // Create userSession Variable of type UserSession with initial value of .loggedOut
+    
+    
+    // Subscribe to receive next events from userSession
+    
+    
+    func logInWith(username: String, password: String, completion: (Error?) -> Void) {
+        guard username == "johnny@appleseed.com",
+            password == "appleseed"
+            else {
+                completion(LoginError.invalidCredentials)
+                return
+        }
+        
+        // Update userSession
+        
+    }
+    
+    func logOut() {
+        // Update userSession
+        
+    }
+    
+    func performActionRequiringLoggedInUser(_ action: () -> Void) {
+        // Ensure that userSession is loggedIn and then execute action()
+        
+    }
+    
+    for i in 1...2 {
+        let password = i % 2 == 0 ? "appleseed" : "password"
+        
+        logInWith(username: "johnny@appleseed.com", password: password) { error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            print("User logged in.")
+        }
+        
+        performActionRequiringLoggedInUser {
+            print("Successfully did something only a logged in user can do.")
+        }
+    }
+}
